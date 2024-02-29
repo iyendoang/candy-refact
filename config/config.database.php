d3101e7654bdefae2706ca496e3b526f3d6bd191
iyendoang <ajun190909@gmail.com>
2024-02-29 16:43:00 +0700

    fix image

--- config/config.database.php
+++ config/config.database.php
@@ -16,9 +16,6 @@ if ($koneksi) {
 			mysqli_set_charset($koneksi, 'utf8');
 			$sess = mysqli_fetch_array(mysqli_query($koneksi, "SELECT * FROM session WHERE id='1'"));
 			date_default_timezone_set($setting['waktu']);
-			$avatar ='dist/img/avatar_default.jpg';
-			$logo = $setting['logo'] != null ? $setting['logo'] : 'dist/img/book.ico';
-			$image_background =  $setting['bc'] != null ? $setting['bc'] : './dist/img/bc.jpg';
 		}
 	}
 }
